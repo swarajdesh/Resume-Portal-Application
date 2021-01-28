47c84a62effd83380bd1840c5dc01e0e8b3d042b
swaraj <swarajdeshmukh45@gmail.com>
2021-01-29 01:05:17 +0530

    Fix error in data.sql

--- Resume-Portal/src/main/java/com/swarajdeshmukh/ResumePortal/models/UserProfile.java
+++ Resume-Portal/src/main/java/com/swarajdeshmukh/ResumePortal/models/UserProfile.java
@@ -1,6 +1,8 @@
 package com.swarajdeshmukh.resumeportal.models;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table
@@ -11,6 +13,66 @@ public class UserProfile {
     private String userName;
     private int theme;
     private String summary;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String designation;
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+
+    @OneToMany(cascade = CascadeType.ALL,
+        orphanRemoval = true)
+            //jobs are associated with profile so whatever happens to profile need to happen to jobs
+    @JoinColumn(name = "job_id")
+    List<Job> jobs = new ArrayList<>();
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
 
     public String getUserName() {
         return userName;
