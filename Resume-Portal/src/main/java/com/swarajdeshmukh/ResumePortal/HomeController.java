c771f2f13bc6bfe40566b340ae94e58832375afe
swaraj <swarajdeshmukh45@gmail.com>
2021-02-01 19:08:56 +0530

    Fixing issues with initializing experience data

--- Resume-Portal/src/main/java/com/swarajdeshmukh/ResumePortal/HomeController.java
+++ Resume-Portal/src/main/java/com/swarajdeshmukh/ResumePortal/HomeController.java
@@ -4,14 +4,18 @@ import com.swarajdeshmukh.resumeportal.models.Job;
 import com.swarajdeshmukh.resumeportal.models.User;
 import com.swarajdeshmukh.resumeportal.models.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.swing.text.html.Option;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 /**@author : Swaraj Deshmukh
@@ -27,14 +31,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(){
-        UserProfile profile1 = new UserProfile();
-        profile1.setId(1);
-        profile1.setDesignation("Designation");
-        profile1.setUserName("einstein");
-        profile1.setFirstname("Albert");
-        profile1.setLastname("Einstein");
-        profile1.setTheme(1);
+        Optional<UserProfile> profileOptional =  userProfileRepository.findByuserName("einstein");
+        profileOptional.orElseThrow(() -> new RuntimeException("Not found " ));
 
+        UserProfile profile1 = profileOptional.get();
         Job job1 = new Job();
         job1.setCompany("Company 1");
         job1.setDesignation("Designation");
@@ -48,7 +48,14 @@ public class HomeController {
         job2.setId(2);
         job2.setStartDate(LocalDate.of(2019,5,1));
         job2.setEndDate(LocalDate.of(2020,1,1));
-        profile1.setJobs(Arrays.asList(job1, job2));
+
+        List<Job> jobs = new ArrayList<Job>();
+        jobs.add(job1);
+        jobs.add(job2);
+        profile1.getJobs().clear();
+        profile1.getJobs().add(job1);
+        profile1.getJobs().add(job2);
+
 
         userProfileRepository.save(profile1);
 
