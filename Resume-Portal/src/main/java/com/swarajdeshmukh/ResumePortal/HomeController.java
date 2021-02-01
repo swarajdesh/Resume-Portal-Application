a0c2fef3ad357bf482b09d8eb35f7543a606b0c7
swaraj <swarajdeshmukh45@gmail.com>
2021-02-01 21:37:13 +0530

    profile-edit.html, Setting up profile edit

--- Resume-Portal/src/main/java/com/swarajdeshmukh/ResumePortal/HomeController.java
+++ Resume-Portal/src/main/java/com/swarajdeshmukh/ResumePortal/HomeController.java
@@ -1,5 +1,6 @@
 package com.swarajdeshmukh.resumeportal;
 
+import com.swarajdeshmukh.resumeportal.models.Education;
 import com.swarajdeshmukh.resumeportal.models.Job;
 import com.swarajdeshmukh.resumeportal.models.User;
 import com.swarajdeshmukh.resumeportal.models.UserProfile;
@@ -7,11 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.swing.text.html.Option;
+import java.security.Principal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -41,6 +41,11 @@ public class HomeController {
         job1.setId(1);
         job1.setStartDate(LocalDate.of(2020,1,1));
         job1.setEndDate(LocalDate.of(2020,3,1));
+        job1.getResponsibilities().add("Theory of relativity and neuclear bomb");
+        job1.getResponsibilities().add("Also made contrubutions to physics ");
+        job1.getResponsibilities().add("Also made contrubutions to quantum mechanics ");
+
+        job1.setCurrentJob(true);
 
         Job job2 = new Job();
         job2.setCompany("Company 2");
@@ -48,23 +53,72 @@ public class HomeController {
         job2.setId(2);
         job2.setStartDate(LocalDate.of(2019,5,1));
         job2.setEndDate(LocalDate.of(2020,1,1));
+        job2.getResponsibilities().add("Theory of relativity and neuclear bomb");
+        job2.getResponsibilities().add("Also made contrubutions to physics ");
+        job2.getResponsibilities().add("Also made contrubutions to quantum mechanics ");
 
-        List<Job> jobs = new ArrayList<Job>();
-        jobs.add(job1);
-        jobs.add(job2);
+
+//        List<Job> jobs = new ArrayList<Job>();
+//        jobs.add(job1);
+//        jobs.add(job2);
         profile1.getJobs().clear();
         profile1.getJobs().add(job1);
         profile1.getJobs().add(job2);
 
+        Education e1 = new Education();
+        e1.setCollege("Harvard College");
+        e1.setQualification("Masters");
+        e1.setSummary("Worked a lot on my thesis");
+        e1.setStartDate(LocalDate.of(2019,5,1));
+        e1.setEndDate(LocalDate.of(2020,1,1));
+
+        Education e2 = new Education();
+        e2.setCollege("Harvard College");
+        e2.setQualification("Phd");
+        e2.setSummary("Worked a lot on my thesis");
+        e2.setStartDate(LocalDate.of(2019,5,1));
+        e2.setEndDate(LocalDate.of(2020,1,1));
+
+        profile1.getEducations().clear();
+        profile1.getEducations().add(e1);
+        profile1.getEducations().add(e2);
+        profile1.getSkills().clear();
+        profile1.getSkills().add("Problem Solving");
+        profile1.getSkills().add("Reading");
+        profile1.getSkills().add("Breaking laws of physics");
+
 
         userProfileRepository.save(profile1);
+
+
 
         return "profile";
     }
 
     @GetMapping("/edit")
-    public String edit(){
-        return "edit page";
+    public String edit(Model model, Principal principal, @RequestParam(required = false) String add){
+        String userId = principal.getName();
+        model.addAttribute("userId", userId);
+        Optional<UserProfile> userProfileOptional = userProfileRepository.findByuserName(userId); //fetch object from database pass the userId
+        userProfileOptional.orElseThrow(() -> new RuntimeException("Not found: " + userId));
+        UserProfile userProfile = userProfileOptional.get(); //get the user profile and set to model attribute
+        if ("job".equals(add)) {
+            userProfile.getJobs().add(new Job());
+        } else if ("education".equals(add)) {
+            userProfile.getEducations().add(new Education());
+        } else if ("skill".equals(add)) {
+            userProfile.getSkills().add("");
+        }
+
+        model.addAttribute("userProfile", userProfile);
+        return "profile-edit";
+    }
+
+    @PostMapping("/edit") //post request to same URL
+    public String postEdit(Model model, Principal principal){
+        String userId = principal.getName();
+        //model.addAttribute("userId", principal.getName());
+        return "redirect:/view/" + userId;  //redirect to user profile
     }
 
     @GetMapping("/view/{userId}")
